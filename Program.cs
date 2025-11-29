c1c0f2ad65ab4ff9fbac60cc6577d5b4bd1abc7e
Neli Hovanesyan <neli_hov@yahoo.com>
2025-11-29 19:46:21 +0200

    Additional code

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
-﻿namespace Methods
+﻿using System.Collections;
+
+namespace Methods
 {
     internal class Program
     {
 
-        public static void CalculateBiggestElementInArray() {
-            int[] arrayOfNumbers = {2, 100, 66, 8, 12, 999};
+        public static void CalculateBiggestElementInArray()
+        {
+            int[] arrayOfNumbers = { 2, 100, 66, 8, 12, 999 };
             int maxNumber = arrayOfNumbers.Max();
             Console.WriteLine(maxNumber);
+            Array.Sort(arrayOfNumbers);
+            Array.Reverse(arrayOfNumbers);
+            foreach (int number in arrayOfNumbers)
+            {
+                Console.WriteLine(number);
             }
-        static void Main(string[] args)
-        {
-            CalculateBiggestElementInArray();
         }
+            static void Main(string[] args)
+            {
+                CalculateBiggestElementInArray();
+            }
+        }
+
     }
-}
