ee7cc4fa6e6627dd8a1b1f65328b4a52450c2d60
Neli Hovanesyan <neli_hov@yahoo.com>
2025-11-29 19:31:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+
+        public static void CalculateBiggestElementInArray() {
+            int[] arrayOfNumbers = {2, 100, 66, 8, 12, 999};
+            int maxNumber = arrayOfNumbers.Max();
+            Console.WriteLine(maxNumber);
+            }
+        static void Main(string[] args)
+        {
+            CalculateBiggestElementInArray();
+        }
+    }
+}
